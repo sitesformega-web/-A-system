a593e2be5abe92895e124545e8cffe0d6e594d6c
sitesformega-web <sitesformega@gmail.com>
2026-03-24 15:07:56 -0300

    Update asi.js

--- assets/js/asi.js
+++ assets/js/asi.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
-  API_URL: "PEGÁ_ACÁ_TU_EXEC",
+  API_URL: https://script.google.com/macros/s/AKfycbyHX9oatOYwjOrQ3taN38eQKcr5wl0g2KhtxELkJQSSJjsy0QFekVL8t9DhQUYCarn57w/exec,
   brandName: "ASTREA",
-  brandTitle: "@TuEmpresa Catálogo",
+  brandTitle: "Catálogo",
   phone: "595971506233",
   logoUrl: "",
   logoLink: "",
