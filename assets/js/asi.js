21236884429f406381f0cea2b452cef4ad413cfc
sitesformega-web <sitesformega@gmail.com>
2026-03-24 15:38:56 -0300

    Update asi.js

--- assets/js/asi.js
+++ assets/js/asi.js
@@ -1,9 +1,21 @@
+export const CATALOG_URL = "https://script.google.com/macros/s/AKfycbyHX9oatOYwjOrQ3taN38eQKcr5wl0g2KhtxELkJQSSJjsy0QFekVL8t9DhQUYCarn57w/exec";
+
 export const CONFIG = {
-  API_URL: https://script.google.com/macros/s/AKfycbyHX9oatOYwjOrQ3taN38eQKcr5wl0g2KhtxELkJQSSJjsy0QFekVL8t9DhQUYCarn57w/exec,
   brandName: "ASTREA",
-  brandTitle: "Catálogo",
+  brandTitle: "@TuEmpresa Catálogo",
+  footerText: "ASTREA © 2026",
   phone: "595971506233",
+
   logoUrl: "",
   logoLink: "",
-  socials: {}
+
+  socials: {
+    instagram: "",
+    facebook: "",
+    tiktok: ""
+  }
+};
+
+export const STORAGE_KEYS = {
+  cart: "pedido_astrea_vitrina"
 };
